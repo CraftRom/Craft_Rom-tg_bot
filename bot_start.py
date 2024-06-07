78204c015946d2d8bc1cc8e32d89bf639f47a1e7
Galitsky Dmytro <melles.android@gmail.com>
2024-06-08 00:09:57 +0300

    Added support for replying to mentions and replies to the bot's messages

--- bot_start.py
+++ bot_start.py
@@ -1,4 +1,5 @@
 import os
+import random
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from commands import start, rom
@@ -11,13 +12,27 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+# Варіанти відповідей
+responses = [
+    "Yes, you tagged me. I'm here!",
+    "Hello! I'm here, ready to assist you.",
+    "You called? I'm ready to help.",
+    "Here I am! What can I do for you?",
+    "Hi! I'm here, ready to answer your questions."
+]
+
 
 # Обробник повідомлень, що містять згадку про бота
 def mention_handler(update, context):
     bot_username = context.bot.username
     if bot_username in update.message.text:
-        # Відповідь на повідомлення, що бота тегнули
-        update.message.reply_text("Так, ви тегнули мене. Я тут!")
+        # Випадково вибрати відповідь і надіслати її
+        selected_response = random.choice(responses)
+        update.message.reply_text(selected_response)
+    elif update.message.reply_to_message and update.message.reply_to_message.from_user.username == bot_username:
+        # Відповідь на повідомлення від бота
+        selected_response = random.choice(responses)
+        update.message.reply_text(selected_response)
 
 
 mention_trigger = MessageHandler(Filters.text & (~Filters.command), mention_handler)
