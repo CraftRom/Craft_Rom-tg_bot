717ae813d5c4b8639fa705e2247b70efa9306614
Galitsky Dmytro <melles.android@gmail.com>
2024-06-09 00:58:54 +0300

    Update run_bot.sh

--- run_bot.sh
+++ run_bot.sh
@@ -7,21 +7,20 @@ echo "Starting bot script at $(date)" >> $LOGFILE
 # Переконайтеся, що знаходитесь в правильній директорії
 cd /home/ubuntu/tg_bot || { echo "Failed to change directory to /home/ubuntu/tg_bot" >> $LOGFILE; exit 1; }
 
-# Створіть та активуйте віртуальне середовище
-if [ ! -d ".venv" ]; then
-python3 -m venv /home/ubuntu/tg_bot/.venv || { echo "Failed to create virtual environment" >> $LOGFILE; exit 1; }
-fi
-
-source /home/ubuntu/tg_bot/.venv/bin/activate || { echo "Failed to activate virtual environment" >> $LOGFILE; exit 1; }
-
 # Встановіть залежності, якщо ще не встановлені
-if [ ! -f ".venv/requirements_installed" ]; then
-    echo "Installing dependencies..."
-    sudo apt install -y python3-pip python3-venv
-    pip install --upgrade pip
-    pip install -r requirements.txt >> $LOGFILE 2>&1 || { echo "Failed to install requirements" >> $LOGFILE; exit 1; }
-    touch .venv/requirements_installed
+if ! command -v python3 &>/dev/null; then
+    echo "Installing Python 3..."
+    sudo apt install -y python3
 fi
+
+if ! command -v pip &>/dev/null; then
+    echo "Installing pip..."
+    sudo apt install -y python3-pip
+fi
+
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt >> $LOGFILE 2>&1 || { echo "Failed to install requirements" >> $LOGFILE; exit 1; }
 
 # Запустіть ваш бот
 python3 bot_start.py >> $LOGFILE 2>&1 || { echo "Failed to start bot" >> $LOGFILE; exit 1; }
