7dbea113bf5a664270dc176b3e9e4da8d25fe828
Galitsky Dmytro <melles.android@gmail.com>
2024-06-10 02:13:55 +0300

    Update filter_messages.py

--- filter_messages.py
+++ filter_messages.py
@@ -5,11 +5,12 @@ from utils import load_topic_id
 
 async def filter_messages(update: Update, context: CallbackContext):
     proposal_thread_id = load_topic_id()
+    thread_id = update.message.message_thread_id
 
     # Фільтрування повідомлень за топіком
-    if proposal_thread_id and update.message.message_thread_id == proposal_thread_id:
+    if thread_id == proposal_thread_id:
         if "#пропозиція" in update.message.text or "#proposal" in update.message.text or "#suggestion" in update.message.text:
-            await update.message.reply_text("Your message has been accepted as a suggestion. We will review it.")
+            await update.message.reply_text("Your message has been accepted as a suggestion. We will review it.", message_thread_id=proposal_thread_id)
         else:
             await context.bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
 
